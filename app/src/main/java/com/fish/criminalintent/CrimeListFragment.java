be7dae3fe156b21a43fc4170cbca5797c465ec22
flyingfishzxf <flyingfishzxf@gmail.com>
2018-12-14 10:27:36 +0800

    10.3 刷新显示列表项

--- app/src/main/java/com/fish/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/fish/criminalintent/CrimeListFragment.java
@@ -85,12 +85,22 @@ public class CrimeListFragment extends Fragment {
         }
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
     private void updateUI() {
         CrimeLab crimeLab = CrimeLab.get(getActivity());
         List<Crime> crimes = crimeLab.getCrimes();
 
-        mAdapter = new CrimeAdapter(crimes);
-        mCrimeRecyclerView.setAdapter(mAdapter);
+        if (mAdapter == null) {
+            mAdapter = new CrimeAdapter(crimes);
+            mCrimeRecyclerView.setAdapter(mAdapter);
+        } else {
+            mAdapter.notifyDataSetChanged();
+        }
     }
 
 }
